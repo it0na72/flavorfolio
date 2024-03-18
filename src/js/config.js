f44ea35ed822f4c9df8ffe77ab5d26163cadc811
Andrew <xgearh3h3@gmail.com>
2024-03-18 21:25:46 +0000

    finished project, will update the logo afterwards

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '6b9200ad-79eb-4753-9360-40fdd8f3ab98';
+export const KEY = '89f43e83-a5a1-4c87-a894-a4f4cdafd9c8';
 export const MODAL_CLOSE_SEC = 2.5;
