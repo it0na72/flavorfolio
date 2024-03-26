eb5a4fe78d4dc3878770eb2eddbea21f4c5b2939
Andrew <xgearh3h3@gmail.com>
2024-03-26 21:04:29 +0000

    updated api url

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = 'd4bd4d7e-a4c4-46cc-b373-6609bf6ae2a4';
